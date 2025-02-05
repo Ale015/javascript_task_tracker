dd6a2b2ce9b71047130dee885599616d59a33059
Ale015 <veadrigojunior@gmail.com>
2025-02-05 18:27:55 -0300

    alterações e implementações das rotas finais

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # javascript_task_tracker
+
+endpoints relevantes da API
+solicitações de exemplo aos endpoints
+implementação em diversas linguagens de programação
+mensagens listadas para os diferentes erros com seus respectivos códigos de status
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 import { createTask } from "../models/taskModel.js";
 
 import mssql from "mssql";
@@ -6,33 +6,79 @@ import mssql from "mssql";
 
 const router = express.Router();
 
+
+
+
+const allowedStatuses = ["not_started","in_progress", "completed"]
+
 // ROTAS
-// GET (Todas as Tarefas)
-router.get("/tasks", async (req, res) => {
 
-    const query = `
-        SELECT * FROM dbo.Tasks
-    `;
+// POST (Nova Tarefa) -> Parâmetro: title, description, status
+router.post("/tasks", async (req,res) =>{
+    const {title, description, status} = req.body;
 
-    async function retrieveData(){
-        const pool = await mssql.connect();
-        const result = await pool.query(query);
-        return result.recordset;
+    const formattedStatus = status.toLowerCase().replace(/\s+/g, "_");
+
+
+    if(!allowedStatuses.includes(formattedStatus)){
+        return res.status(400).json({ message: "Status inválido. Use: not_started, in_progress ou completed." });
     }
 
     try {
-        
-        const tasks = await retrieveData();
-        res.status(200).json(tasks)
-        
+        const result = await createTask(title, description, formattedStatus);
+        res.status(201).json(result); // Garantindo o retorno da criação
     } catch (error) {
-        console.error("Erro ao buscar tarefas:", error);
-        res.status(500).json({ error: "Erro ao buscar tarefas." });
+        res.status(500).json({ message: error.message });
     }
+});
 
 
+
+// GET (Todas as Tarefas)
+// GET (Todas as Tarefas Filtradas por Status) -> Parâmetro: Status
+router.get("/tasks", async (req, res) => {
+    let { status } = req.query;
+
+    let query = `SELECT * FROM dbo.Tasks`
+
+    let request = null;
+
+    try {
+       const pool = await mssql.connect();
+       request = pool.request();
+       
+        if(status){
+            const statusArray = status.split(",").map(s => s.trim().toLowerCase().replace(/\s+/g, "_"));
+            const allowedStatuses  = ["not_started", "in_progress", "completed"];
+            const validStatuses = statusArray.filter(s => allowedStatuses.includes(s));
+
+            if(validStatuses.length === 0){
+                return res.status(400).json({message: "Nenhum status válido encontrado. Use: not_started, in_progress, complete"})
+            }
+
+            const placeholders = validStatuses.map( (value ,index) => `@status${index}`).join(", ");
+            query += ` WHERE status IN (${placeholders})`;
+
+            validStatuses.forEach((value, index) => {
+                request.input(`status${index}`, mssql.NVarChar, value);
+            })
+        }
+
+        const result  = await request.query(query) 
+
+        if(result.recordset.length === 0){
+            return res.status(400).json({message: "Nenhum status válido encontrado. Use: not_started, in_progress, completed."})
+        }
+
+        res.status(200).json(result.recordset);
+
+    } catch (error) {
+        console.error("Erro ao buscar tarefas:", error.message);
+        res.status(500).json({ message: "Erro ao buscar tarefas." });
+    }
     
-})
+});
+
 
 
 
@@ -41,7 +87,7 @@ router.get("/tasks/:id", async (req,res)=>{
 
     const {id} = req.params;
 
-    const query = `SELECT * FROM dbo.tasks WHERE id = @id`;
+    const query = `SELECT * FROM dbo.Tasks WHERE id = @id`;
 
     try {
         const pool = await mssql.connect();
@@ -65,33 +111,83 @@ router.get("/tasks/:id", async (req,res)=>{
 
 })
 
-// GET (Todas as Tarefas Filtradas por Status) -> Parâmetro: Status
-// POST (Nova Tarefa) -> Parâmetro: title, description, status
+
 // PUT (Editar Tarefa já existente) -> Parâmetro: ID e body com atualização
-// PUT (Editar Status Tarefa) -> Parâmetro: ID e Status
-// DELETE (Deletar Tarefa) -> Parâmetro: ID
+router.put("/tasks/:id", async (req, res) => {
+    const {id} = req.params;
+    const updates = req.body;
 
-
-
-
-
-// Rota = Criação de Tarefas 
-
-router.post("/tasks", async (req,res) =>{
-    try {
-        const {title, description, status} = req.body;
-
-        const newTask = await createTask(title, description, status)
-
-        res.status(201).json({ message: "Tarefa criada com sucesso.", task: newTask})
-    } catch (error) {
-        
-        res.status(500).json({error: "Erro ao criar tarefa: ", details})
+    if(!id){
+        return res.status(400).json({ message: "ID da tarefa é obrigatório." })
     }
-});
+
+    if(Object.keys(updates).length === 0){
+        return res.status(400).json({message: "Nenhuma atualização fornecida."})
+    }
+
+    const allowedFields = ["title","description","status"]
+    const updateFields = Object.keys(updates).filter(field => allowedFields.includes(field));
+
+    if(updateFields.length === 0){
+        return res.status(400).json({message: "Campos inválidos para atualização."})
+    }
+
+    const querySet = updateFields.map(field => `${field} = @${field}`).join(", ")
+
+    const query = `
+        UPDATE dbo.Tasks SET ${querySet} WHERE id = @id;
+    `
+
+    try {
+        
+        const pool = await mssql.connect();
+        const request = await pool.request();
+
+        request.input("id", mssql.Int, id);
+        updateFields.forEach(field => request.input(field, mssql.NVarChar, updates[field]))
+
+        const result = await request.query(query);
+
+        if (result.rowsAffected[0] === 0){
+            return res.status(404).json({message: "Tarefa não encontrada."})
+        }
+
+        res.status(200).json({ message: `Tarefa de ID ${id} atualizada com sucesso.` })
+
+    } catch (error) {
+        console.error("Erro ao atualizar tarefa:", error);
+        res.status(500).json({ message: "Erro ao atualizar tarefa." });
+    }
 
 
+})
 
+// DELETE (Deletar Tarefa) -> Parâmetro: ID
+router.delete("/tasks/:id", async (req, res) => {
+
+    const {id} = req.params;
+
+    const query = `
+        DELETE FROM dbo.Tasks WHERE id = @id
+    `;
+
+    try {
+        const pool = await mssql.connect();
+        const result = await pool.request().input("id", mssql.Int, id).query(query);
+
+        if (result.rowsAffected[0] === 0) {
+            return res.status(404).json({ message: `Tarefa com ID ${id} não encontrada.` });
+        }
+
+
+        res.status(204).send()
+    } catch (error) {
+        console.error("Erro ao tentar deletar tarefa por ID:", error);
+        res.status(500).json({message: "Erro ao deletar tarefa."});
+    }
+
+
+})
 
 
 
