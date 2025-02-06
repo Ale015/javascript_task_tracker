064398b195e163fbcebe07ab168fab30bdc64fa1
Ale015 <veadrigojunior@gmail.com>
2025-02-06 15:19:14 -0300

    adição do cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import {connectDB} from "./db.js";
 import dotenv from "dotenv"
 import express from "express";
 
+import cors from "cors"
 
 import taskRoutes from "./routes/taskRoutes.js"
 
@@ -13,6 +14,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/api", taskRoutes)
+
+app.use(cors())
 
 connectDB();
 
