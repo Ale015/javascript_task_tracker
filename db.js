a9b2900a9f71d864f7cc6bf73105af9833af7daf
Ale015 <veadrigojunior@gmail.com>
2025-02-05 22:25:53 -0300

    retirar tipagem DB_PORT

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const dbConfig = {
     password: process.env.DB_PASSWORD,
     server: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
-    port: parseInt(process.env.DB_PORT),
+    port: process.env.DB_PORT,
     options: {
         encrypt: false,
         trustServerCertificate: true
