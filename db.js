fbca3d87c317a30f2d4aa2e9f3dee8e23b67b0ee
Ale015 <veadrigojunior@gmail.com>
2025-02-05 22:16:40 -0300

    alteração DB_Server pra DB_Host

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
+    server: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     port: parseInt(process.env.DB_PORT, 10),
     options: {
