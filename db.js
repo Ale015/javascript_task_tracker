24246290f5016976e5d090c1ba1ad4d612512896
Ale015 <veadrigojunior@gmail.com>
2025-02-05 22:33:20 -0300

    alter db_host

--- db.js
+++ db.js
@@ -3,12 +3,20 @@ import mssql from "mssql";
 
 dotenv.config();
 
+
+// Verificação das variáveis de ambiente
+if (!process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_HOST || !process.env.DB_DATABASE || !process.env.DB_PORT) {
+    console.error("Erro: Variáveis de ambiente faltando!");
+    process.exit(1); // Para garantir que o código pare aqui se faltar alguma variável
+}
+
+
 const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     server: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
+    port: parseInt(process.env.DB_PORT,10),
     options: {
         encrypt: false,
         trustServerCertificate: true
